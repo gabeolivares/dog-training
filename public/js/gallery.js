6652cb7b9f2e13fc5d891b0164bfe3b2c6ac07bb
Rudolph Newball <newrdev@gmail.com>
2016-07-14 20:03:32 -0500

    Basic gallery section

---
+++ public/js/gallery.js
@@ -0,0 +1,7 @@
+(function($) {
+    "use strict";
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+})(jQuery);
