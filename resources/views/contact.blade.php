c0a95da7839fa2a3fe53d7dc9a16f49012e6d5b4
Rudolph Newball <newrdev@gmail.com>
2016-07-16 16:30:29 -0500

    Basic contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,55 @@
 @extends('layout')
 
-@section('header')
-<!-- Load specific CSS  -->
+@section('head')
+<link rel="stylesheet" href="/css/contact.css">
 @stop
 
 @section('content')
 
-<h1>Contact Page</h1>
+<section id="contact">
+  <h1>Contact Page</h1>
+  <div class="container">
+    <div class="row header">
+      <h1>CONTACT US &nbsp;</h1>
+      <h3>Fill out the form below to learn more!</h3>
+    </div>
+    <div class="row body">
+      <form action="#">
+        <ul>
 
+          <li>
+            <p class="left">
+              <label for="first_name">first name</label>
+              <input type="text" name="first_name" placeholder="John" />
+            </p>
+            <p class="pull-right">
+              <label for="last_name">last name</label>
+              <input type="text" name="last_name" placeholder="Smith" />
+            </p>
+          </li>
+
+          <li>
+            <p>
+              <label for="email">email <span class="req">*</span></label>
+              <input type="email" name="email" placeholder="john.smith@gmail.com" />
+            </p>
+          </li>
+          <li><div class="divider"></div></li>
+          <li>
+            <label for="comments">comments</label>
+            <textarea cols="46" rows="3" name="comments"></textarea>
+          </li>
+
+          <li>
+            <input class="btn btn-submit" type="submit" value="Submit" />
+            <small>or press <strong>enter</strong></small>
+          </li>
+
+        </ul>
+      </form>
+    </div>
+  </div>
+</section>
 @stop
 
 @section('footer')
