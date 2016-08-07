df9f82fc7aabe445169ad8a47c3a545d6fdcc708
Rudolph Newball <newrdev@gmail.com>
2016-08-07 17:41:09 -0500

    About page text + css

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,67 @@
 
 @section('content')
 
-<h1>About Page</h1>
+<style>
+  .team-info {
+    padding: 40px 30px 10px 30px;    
+  }
+</style>
+
+<section>
+  <div class="text">
+    <h1>About Our Facility</h1>
+    <p>
+      The Barkin' Bones Inn offers an astounding 261,360 sqft facility dedicated solely to dogs.
+      With several secured play yards, all dogs are bound to have a bark-worthy good time.
+      From water yards, tug-a-war, play tag, shuffling, sniffing, sunbathing, jumping, and romping with toys
+      we have amenities to fit any character and personality of dog.
+      We tailor to specific needs of dogs whether its a senior dog that enjoys shady walks to young puppies
+      that want to run, run, run. *Rabies, Distemper, &amp; bordetella is required.
+    </p>
+  </div>
+
+  <img src="http://www.awesomelycute.com/gallery/2014/12/dog-families-10.jpg" alt="About us image" />
+
+  <div class="text">
+    <h1>Meet The Crew</h1>
+
+    <div class="row">
+      <div class="col-md-6 team-info bg-dark">
+        <img src="//res.cloudinary.com/thebarkinbonesinn/image/upload/v1470604826/contact/candi.jpg" al="Candi" />
+
+        <h3>Candi-La'Rue Smith- Owner</h4>
+        <p>
+          Destined to actively work with dogs, Candi was inspired to tailor to
+          dogs needs as a young girl. Accompanied with 4 adorable cats, 2 loyal German Shepherds, and a lovable
+          Retriever- Candi's lifestyle is naturally about caring for animals.  Training and working with dogs since
+          the age of 12, she is experienced, professional, and knowledgeable. "Growing up I always told my mom I
+          wanted a piece of land to have dogs come over and slumber with me when their parents went out of town."
+          Customizing a broad range of extra curricular programs for dogs to enjoy while being away from home.
+          With successfully training over 100 dogs, expert in behavior modification, and dog socialization- her
+          small business has flourished.
+        </p>
+      </div>
+
+      <div class="col-md-6 team-info bg-dark">
+        <img src="//res.cloudinary.com/thebarkinbonesinn/image/upload/v1470604828/contact/stephanie.jpg" al="Stephanie" />
+
+        <h3>Stephanie Fletcher-Performance Manager</h4>
+        <p>
+          Stephanie has been working with Candi for over 3 years. She grew up in a small town in Idaho with a
+          passion to work with animals. From dogs to horses she is experienced with most breeds. Stephanie also
+          enjoys working with young children at a local charter school as an after school enrichment group leader.
+          She is hard working, dedicated, and  attentive to dogs in her care. A proud mother of an adorable little
+          boy who she hopes carries on a passion for animals. Stephanie does not currently have any fur children,
+          but hopes to find a perfect match in the future.
+        </p>
+      </div>
+
+    </div>
+
+
+  </div>
+
+</section>
 
 @stop
 
