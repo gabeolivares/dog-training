846e739490daac4ffa7bd4dfa258d17986d75a11
Rudolph Newball <newrdev@gmail.com>
2016-08-15 23:30:09 -0500

    Fix footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,18 +1,19 @@
-<div id="copyright text-right">© Copyright 2016 The Barkin' Bones Inn</div>
+<div class="footer">
+  <div id="copyright text-right">© Copyright 2016 The Barkin' Bones Inn</div>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script src="/js/navbar.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+  <script src="/js/navbar.js"></script>
 
-<script>
-(function($) {
-  "use strict";
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
-  console.log("finished setting it up");
-})(jQuery);
-</script>
+  <script>
+  (function($) {
+    "use strict";
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
+  })(jQuery);
+  </script>
+</div>
