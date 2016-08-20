771d0f46bc992c50a2013831d976e3011d80a5ba
Rudolph Newball <newrdev@gmail.com>
2016-08-19 23:58:21 -0500

    Update grooming UI

--- resources/views/services/grooming.blade.php
+++ resources/views/services/grooming.blade.php
@@ -23,54 +23,93 @@
 
   <hr />
 
-  <h3>Packages</h3>
+  <h2>Packages</h2>
   <div class="row descriptions">
-    <div class="col-md-4">
-      <h4>Organic Rinse</h4>
-      <p>Basic wash($12-$18 depending on length of coat)</p>
+    <div class="col-md-3 offer">
+      <h3>Homemade Organic Rinse</h3>
+      <p>A homemade rinse to clean and moisturize coat and skin</p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>$12-$18 depending on length of coat</li></ul>
+      </div>
     </div>
 
-    <div class="col-md-4">
-      <h4>Deluxe Spa Package</h4>
+    <div class="col-md-3 offer">
+      <h3>Original Spa Package</h3>
+      <p>Homemade shampoo scrub, blow dry, &amp; brush</p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>$25-$35 based of coat length</li></ul>
+      </div>
+    </div>
+
+    <div class="col-md-3 offer">
+      <h3>Deluxe Spa Package</h3>
       <p>
         Homemade shampoo scrub, 10minute homemade condition set, ear cleaning,
-        nail grind, blow dry, deshed, soft brush, and nose and paw moisturizer($45-$60 based of coat length)
+        nail grind, blow dry, deshed, soft brush, and nose and paw moisturizer
       </p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>45-$60 based of coat length</li></ul>
+      </div>
     </div>
 
-    <div class="col-md-4">
-      <h4>Basic Spa Package</h4>
-      <p>Homemade shampoo scrub, blow dry, &amp; brush ($25-$35 based of coat length)</p>
+    <div class="col-md-3 offer">
+      <h3>Ear &amp; Paw Package</h3>
+      <p>This consist of Nail grinding + Ear cleaning</p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>$20</li></ul>
+      </div>
     </div>
   </div>
 
   <hr />
 
-  <h3>Stand Alone Services</h3>
+  <h2>Stand Alone Services</h2>
   <div class="row descriptions">
-    <div class="col-md-6">
-      <h4>Dry Brushing</h4>
-      <p>$10( deep brushing without bath) for dogs needing extra brushing this can be added to spa package</p>
+    <div class="col-md-offset-1 col-md-5 offer">
+      <h3>Dry Brushing</h3>
+      <p>For dogs needing extra brushing this can be added to spa package</p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>$10 deep brushing without bath</li></ul>
+      </div>
     </div>
 
-    <div class="col-md-6">
-      <h4>Wrinkle Maintenance</h4>
-      <p>$5 wipe facial/body wrinkles tailor for bulldogs, Boston terriers, pugs, sharpei's, mastiffs, boxers, etc</p>
+    <div class="col-md-5 offer">
+      <h3>Wrinkle Maintenance</h3>
+      <p>Tailored for bulldogs, Boston Terriers, Pugs, Sharpei's, Mastiffs, Boxers, etc</p>
+
+      <div>
+        <span>Rate:</span>
+
+        <ul><li>$5 wipe facial/body wrinkles</li></ul>
+      </div>
     </div>
   </div>
+
   <div class="row descriptions">
-    <div class="col-md-4">
-      <h4>Ear &amp; Paw</h4>
-      <p>$20</p>
+
+
+    <div class="col-md-offset-4 col-md-2 offer">
+      <h3>Nail Grind</h3>
+      <p>$15 (up-charge for longer nails)</p>
     </div>
 
-    <div class="col-md-4">
-      <h4>Nail Grind</h4>
-      <p>$15(up-charge for longer nails)</p>
-    </div>
-
-    <div class="col-md-4">
-      <h4>Ear Cleaning</h4>
+    <div class="col-md-2 offer">
+      <h3>Ear Cleaning</h3>
       <p>$6-$12</p>
     </div>
   </div>
