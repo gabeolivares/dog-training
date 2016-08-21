f002b9882b07ae031db6eb710913c3e3c7f40fc6
Rudolph Newball <newrdev@gmail.com>
2016-08-20 20:13:39 -0500

    Make sure we take mobile viewports

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="description" content="">
 <meta name="author" content="The Barkin' Bones Inn">
+<meta name="viewport" content="width-device-width,initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>The Barkin' Bones Inn</title>
