46e45d59db6e80834a0ceb9516b176cd2b3d0c30
Rudolph Newball <newrdev@gmail.com>
2016-08-20 20:13:29 -0500

    Update training UI

--- resources/views/services/training.blade.php
+++ resources/views/services/training.blade.php
@@ -25,26 +25,144 @@
   <hr />
 
   <div class="row descriptions">
-    <div class="col-md-3">
-      <h4>Special Trick Class</h4>
-      <p>$50/session</p>
+    <div class="col-md-4 offer">
+      <h3>Customizable Training- Behavior Modification- Trick Training</h3>
+      <p>Custom training session lasting between 60-75 minutes. This session does not include training provided by the obedience packages such as:</p>
+      <ul>
+        <li>Out of sight Stays</li>
+        <li>Street Avoidance</li>
+        <li>Play Dead</li>
+        <li>Swimming Lessons</li>
+        <li>Potty Training</li>
+        <li>Bathroom Bell</li>
+        <li>Barking/Digging Issues</li>
+      </ul>
+
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>$50/session</li></ul>
+      </div>
     </div>
 
-    <div class="col-md-3">
-      <h4>Obedience(Basic-Supreme)</h4>
-      <p>$35-$50 per session. Sessions at 60-75min long and last from 4-8weeks</p>
+    <div class="col-md-4 offer">
+      <h3>Obedience</h3>
+      <p>
+        We offer several levels of obedience training session.
+        Sessions are between 60-75 minutes long and last from 4-8 weeks.
+      </p>
+
+      Basic (3 levels) Bronze, Silver, Gold:
+      <ul>
+        <li>Sit</li>
+        <li>Stay</li>
+        <li>Come</li>
+        <li>Leave It</li>
+        <li>Name</li>
+        <li>Leash Etiquette</li>
+      </ul>
+
+      Supreme Obedience
+      <br />
+      <i style="font-size:15px">*Must qualify for this class*</i>
+      <ul>
+        <li>Car Etiquette</li>
+        <li>Front Door/Leash Manners</li>
+        <li>Place</li>
+        <li>Heel</li>
+        <li>Sign Language</li>
+        <li>Focus</li>
+      </ul>
+
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>$35-$50/session</li></ul>
+      </div>
     </div>
 
-    <div class="col-md-3">
-      <h4>Person Protection</h4>
-      <p>Inquire</p>
-    </div>
+    <div class="col-md-4 offer">
+      <h3>Blitz Training</h3>
+      <i style="font-size:15px">*Must qualify for this class*</i>
+      <p>
+        This is recommend for experienced dog owners in need of a refresher to previous training,
+        adding another dog to the family, adoption, taking on a young adult dog or puppy, or wanting
+        to provide additional structure with out taking a 6-8 week course over again.
+        We exercise healthy dog habits and manners. Reinforce crate manners, basic obedience, and grooming practices.
+        This training is not for new dog owners or less experienced dog owners
+      </p>
 
-    <div class="col-md-3">
-      <h4>Walking on a leash/leash reactivity</h4>
-      <p>$45</p>
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>$150/2hr session</li></ul>
+      </div>
     </div>
   </div>
+
+  <div class="row descriptions">
+    <div class="col-md-4 offer">
+      <h3>Board N' Train</h3>
+      <p>
+        This services offers many unique features.
+        A BnT is a 14-21 day training where the dog stays at our facility and gets
+        around the clock training. Our BnT's that we currently offer are:
+      </p>
+      <ul>
+        <li>Crate Training</li>
+        <li>Potty Training</li>
+        <li>Healing Anxiety</li>
+        <li>Behavior modification</li>
+        <li>Aggression (human/Dog/Leash)</li>
+      </ul>
+
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>$65/day</li></ul>
+      </div>
+    </div>
+
+    <div class="col-md-4 offer">
+      <h3>Person Protection</h3>
+      <p>
+        Classes consist of a balanced dog (breed is irrelevant) Owner dedication,
+        and much practice in many different scenarios.
+      </p>
+
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>Please inquire for rates</li></ul>
+      </div>
+    </div>
+
+    <div class="col-md-4 offer">
+      <h3>Leash Training</h3>
+      <p>
+        Generally a 60-75minute course focusing on just working our dogs on leash.
+        Leash training consists of:
+      </p>
+      <ul>
+        <li>Pulling on a Leash</li>
+        <li>Determining the right equipment(leash, harness, collar, Etc)</li>
+        <li>How to correctly put the leash on a dog(dog being able to control his happiness to go for a walk)</li>
+        <li>Leash Reactivity - barking at cars, dogs, humans, etc</li>
+      </ul>
+
+      <div class="rate">
+        <span>Rate:</span>
+
+        <ul><li>$45/session</li></ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="price-change">
+    *Please note that all of our training is "new innovation". We do not use choke collars,
+    prong collars, shock/vibration collars, "alpha approaches", domination, or any physical
+    threat to any dog for any duration at any time during training or any other service we offer.
+  </div> -->
 
 </section>
 
