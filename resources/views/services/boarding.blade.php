fae519925bbb3945ec9bd336f0294a08f8feb123
Rudolph Newball <newrdev@gmail.com>
2016-08-23 21:23:09 -0500

    Boarding page text changes

--- resources/views/services/boarding.blade.php
+++ resources/views/services/boarding.blade.php
@@ -26,19 +26,15 @@
 
   <div class="row descriptions">
 
-    <div class="col-md-offset-3 col-md-3 offer">
+    <div class="col-md-4 offer">
       <h3>Overnight Slumber - $35/Night</h3>
-
-      <div>
-        <ul><li></li></ul>
-      </div>
 
       <p>4 potty breaks</p>
       <p>Feeding</p>
       <p>Cozy blanket to sleep on</p>
     </div>
 
-    <div class="col-md-3 offer">
+    <div class="col-md-4 offer">
       <h3>Premium Slumber - <span>$45/Night</spn></h3>
 
       <p>4 potty breaks</p>
@@ -47,21 +43,38 @@
       <p>Plush bed to sleep on</p>
       <p>Daily photo updates</p>
     </div>
+
+    <div class="col-md-4 offer">
+      <h3>Kitchen Boarding - <span>$55/Night</spn></h3>
+
+      <p>Includes all of Premium boarding advantages plus:</p>
+      <p>A crate free environment</p>
+      <p>Elevated poochie cot and toys</p>
+      <p>Constant attention from the crew</p>
+      <p>Pet guest must qualify for Kitchen Boarding and pass a free evaluation.</p>
+      <div class="price-change">*Pet insurance is required for Kitchen Boarding the cost is $50 per fiscal year*</div>
+    </div>
   </div>
 
   <div class="row descriptions">
-    <div class="col-md-offset-4 col-md-4 offer">
-      <h3>Boarding Add-On(s)</h3>
+    <div class="col-md-offset-2 col-md-8 offer">
+      <h3 class="center">Boarding Add-On(s)</h3>
 
-      <p>30 minutes Bark Walk - $15 (Boarding Rate)</p>
-      <p>Early Check In/Check Out - $25 (Holiday Pickup - $40)</p>
-      <p>Photo Updates - $3/day</p>
-      <p>Paw Wax - $3/day</p>
-      <p>Administer Medications - $2/day</p>
-      <p>Insulin/Steroids Injections - $10/day</p>
-      <p>We proudly serve Fromms Gold line dog food - $3/day</p>
-      <p><i>*Inquire about additional slumber upgrades*</i><p>
+      <p>30 minutes Bark Walk - <b>$15 (Boarding Rate)</b></p>
+      <p>Early Check In/Check Out - <b>$25 (Holiday Pickup - $40)</b></p>
+      <p>Photo Updates - <b>$3/day</b></p>
+      <p>Paw Wax - <b>$3/day</b></p>
+      <p>Administer Medications - <b>$2/day</b></p>
+      <p>Insulin/Steroids Injections - <b>$10/day</b></p>
+      <p>We proudly serve Fromms Gold line dog food - <b>$3/day</b></p>
+      <div class="price-change">*Inquire about additional slumber upgrades*</div>
     </div>
+  </div>
+
+  <div class="price-change">
+    Multiple Pet Boarding: <br />
+    When boarding in the same room we offer a 25% discount off the first
+    additional pet &amp; 50% off each subsequent pet.
   </div>
 
 </section>
